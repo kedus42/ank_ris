77fa0cfa520fc98ab2550aa196ecc4145fd56c9f
kedus42 <k.mathewos@jacobs-university.de>
2021-03-16 12:18:45 +0100

    Proper preprocessing and later stage cascades added

--- src/steering.py
+++ src/steering.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python2.7
 import rospy
 import time
-from sensor_msgs.msg import CompressedImage, Joy
+from sensor_msgs.msg import CompressedImage, Joy, Image
+from cv_bridge import CvBridge
 import numpy as np
 
-nduckie=cv2.CascadeClassifier("../haarcascades/duckie_cascade_stage10.xml")
+nduckie=cv2.CascadeClassifier("../haarcascades/duckie_cascade_stage12.xml")
 
 rospy.init_node('steering')
 steering_pub=rospy.Publisher('/ank/joy', Joy, queue_size=30)
+image_pub=rospy.Publisher('/ank/detections', Image, queue_size=30)
 camwidth=50
 camheight=50
 move_threshhold=int(camwidth*.8)
+action_threshhold=5
+bridge=CvBridge()
 
 def callback(image):
     arr=np.fromstring(image.data, np.uint8)
@@ -20,23 +24,42 @@ def callback(image):
     count=0
     while i<8:
         command.axes.append(0)
-    for x,y,w,h in duckie:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 1)
-        cv2.imshow("found this duckie", img)
-        if x+w/2 < int((camwidth/2)-camwidth/10):
-            command.axes[3]=.5#wherever left is
-        elif x+w/2 > int((camwidth/2)+camwidth/10):
-            command.axes[3]=-.5#wherever right is
-        if w < move_threshhold:
-            command.axes[1]=w/(camwidth)
-        elif w > move_threshhold:
-            command.axes[1]=-.2
-        else:
-            command.axes[1]=0
-        steering_pub.publish(command)
+        i+=1
+    for x,y,w,h in duckies:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), 1)
+        if x < lowestx:
+            lowestx=x
+            lowestw=w
+        if x+w > highestx:
+            highestx=x+w
+            highestw=w
+        if y < lowesty:
+            lowesty=y
+            lowesth=h
+        if y+h > highesty:
+            highesty=y+h
+            highesth=h
         count+=1
-        if count =1:
-            break
+    recx=lowestx
+    recy=lowesty
+    recw=highestx-lowestx
+    rech=highesty-lowesty
+    if count>action_threshhold:
+        cv2.rectangle(img, (lowestx, lowesty), (lowestx+recw, lowesty+rech), (0, 255, 0), 1)
+        if recx+recw/2 < int((camwidth/2)-camwidth/10):
+            command.axes[1]=.5
+        elif recx+recw/2 > int((camwidth/2)+camwidth/10):
+            command.axes[3]=-.5
+        if recw < move_threshold:
+            command.axes[1]=1-recw/camwidth
+        elif recw > move_threshold:
+            command.axes[1]=.2
+    else:
+        command.axes[1]=0
+        command.axes[3]=0
+    detect_msg=bridge.cv2_to_imgmsg(img, 'bgr8')
+    image_pub.publish(detect_msg)
+    #steering_pub.publish(command)
 
 camera_sub=rospy.Subscriber('/ank/camera_node/image/compressed', CompressedImage, callback=callback)
 rospy.spin()
