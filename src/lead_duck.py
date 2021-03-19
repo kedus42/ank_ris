06398439d8fb9a332508dfd7d69795c954ebb2ba
kedus42 <k.mathewos@jacobs-university.de>
2021-03-19 23:00:30 +0100

    lead duck driver added

--- src/lead_duck.py
+++ src/lead_duck.py
@@ -2,9 +2,7 @@
 import rospy
 import time
 from sensor_msgs.msg import Joy
-from 
 import numpy as np
-import cv2
 
 rospy.init_node('drive_lead')
 steering_pub=rospy.Publisher('/lead/joy', Joy, queue_size=30)
@@ -12,6 +10,12 @@ steering_pub=rospy.Publisher('/lead/joy', Joy, queue_size=30)
 interval=5
 lead_speed=.6
 command=Joy()
+i=0
+while i<8:
+    command.axes.append(0)
+    i+=1
+command.axes[1]=lead_speed
+start=time.time()
 
 def callback(msg):
     if msg="lost":
@@ -22,13 +26,6 @@ def callback(msg):
         command.axes[3]=np.random.uniform(-.5, .5)
 
 follow_sub=rospy.Subscriber('/lead/lost', String, callback=callback)
-
-i=0
-while i<8:
-    command.axes.append(0)
-    i+=1
-command.axes[1]=lead_speed
-start=time.time()
 
 while 1:
     if start - time.time() > interval
