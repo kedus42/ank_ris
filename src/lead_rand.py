84ac433c6835f7b3481c58e13315db6f873c849e
kedus42 <k.mathewos@jacobs-university.de>
2021-04-09 17:37:10 +0200

    added sigint intercept

--- src/lead_rand.py
+++ src/lead_rand.py
@@ -4,8 +4,9 @@ import time
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String
 import numpy as np
+import signal
 
-rospy.init_node('lead_rand')
+rospy.init_node('lead_rand', disable_signals=True)
 steering_pub=rospy.Publisher('/lead/joy', Joy, queue_size=30)
 
 interval=5
@@ -20,6 +21,11 @@ command.axes[1]=lead_speed
 start=time.time()
 move=True
 
+def sigint(sig):
+    command.axes[1]=0
+    command.axes[3]=0
+    steering_pub.publish(command)
+
 def callback(msg):
     global command, move
     if msg=="lost":
@@ -29,10 +35,11 @@ def callback(msg):
         command.axes[1]=lead_speed
         move=True
 
+signal.signal(signal.SIGINT, sigint)
 follow_sub=rospy.Subscriber('/lead/lost', String, callback=callback)
 
 while 1 and not rospy.is_shutdown():
     if time.time()-start > interval and move:
-        command.axes[3]=np.random.uniform(-.3, .3)
+        command.axes[3]=np.random.uniform(-.1, .1)
         start=time.time()
     steering_pub.publish(command)
